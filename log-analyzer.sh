26db581649b5226f7b82ab8ae7535cf8242518d0
Toluwanee <toluwalope9@gmail.com>
2024-12-17 15:59:17 +0100

    Top 5 most requested path

--- log-analyzer.sh
+++ log-analyzer.sh
@@ -2,8 +2,11 @@
 
 log_file="$1"
 
-echo -e "\nTop 5 IP request: "
+echo -e "\nTop 5 IP request with the most requests: "
 awk '{print $1}' nginx-access.log | sort -n | uniq -c | sort -nr | head -5 | awk '{printf "%s - %s\n", $2, $1}'
+
+echo -e "\nTop 5 most requested paths: "
+awk '{print $7}' nginx-access.log | sort -n | uniq -c | sort -nr | head -5 | awk '{printf "%s - %s\n", $2, $1}'
 
 echo -e "\nTop 5 status request code: "
 awk '{print $9}' nginx-access.log | sort -n | uniq -c | sort -nr | head -5 | awk '{printf "%s - %s\n", $2, $1}'
