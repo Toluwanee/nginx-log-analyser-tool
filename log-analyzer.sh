d1e7665a9de1ca098cf4cee50f468cd2c26f7e3f
Toluwanee <toluwalope9@gmail.com>
2024-12-17 15:57:04 +0100

    Top 5 status request code

--- log-analyzer.sh
+++ log-analyzer.sh
@@ -5,3 +5,5 @@ log_file="$1"
 echo -e "\nTop 5 IP request: "
 awk '{print $1}' nginx-access.log | sort -n | uniq -c | sort -nr | head -5 | awk '{printf "%s - %s\n", $2, $1}'
 
+echo -e "\nTop 5 status request code: "
+awk '{print $9}' nginx-access.log | sort -n | uniq -c | sort -nr | head -5 | awk '{printf "%s - %s\n", $2, $1}'
