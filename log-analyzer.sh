a6ebbe80a9c458eb0f3f450baf2ec64463341db7
Toluwanee <toluwalope9@gmail.com>
2024-12-17 16:01:07 +0100

    Top 5 user agents

--- log-analyzer.sh
+++ log-analyzer.sh
@@ -10,3 +10,6 @@ awk '{print $7}' nginx-access.log | sort -n | uniq -c | sort -nr | head -5 | awk
 
 echo -e "\nTop 5 status request code: "
 awk '{print $9}' nginx-access.log | sort -n | uniq -c | sort -nr | head -5 | awk '{printf "%s - %s\n", $2, $1}'
+
+echo -e "\nTop 5 user agents: "
+awk '{print $12}' nginx-access.log | sort -n | uniq -c | sort -nr | head -5 | awk '{printf "%s - %s\n", $2, $1}'
